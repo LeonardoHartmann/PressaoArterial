87d635440fc6e38593a52e2ad23b7ae7504bb5ef
Leonardo <leonardohartmann03@gmail.com>
2022-09-10 17:16:39 -0300

    PressaoArterial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const MedidorDePressao = () => {
+
+    const [sistolica, setSistolica] = useState('')
+    const [diastolica, setDiastolica] = useState('')
+    const [result, setResult] = useState('')
+
+    
+    const Verificar = () => {
+        if (sistolica < 130 && diastolica < 85) {
+            setResult('Normal')
+        } else if (sistolica < 140 && diastolica < 90) {
+            setResult('Normal - Limítrofe')
+        } else if (sistolica < 160 && diastolica < 100) {
+            setResult('Hipertensão Leve')
+        } else if (sistolica < 180 && diastolica < 110) {
+            setResult('Hipertensão Moderada')
+        } else {
+            setResult('Hipertensão Grave')
+        }
+    }
+
+
+    return (
+        <div>
+            <h1>Controle de Pressão Arterial</h1>
+
+            Pressão sistólica: <br />
+            <input type="number" value={sistolica}
+                onChange={event => setSistolica(event.target.value)} />
+
+            <br />
+            
+            Pressão diastólica: <br />
+            <input type="number" value = {diastolica}
+                onChange={event => setDiastolica(event.target.value)} />
+
+            <br /><br />
+
+            <input type="button" value="Verificar"
+                onClick={() => Verificar(result)} />
+
+            <br /><br />
+
+            {result}
+        </div>
+    )
 }
 
-export default App;
+export default MedidorDePressao
